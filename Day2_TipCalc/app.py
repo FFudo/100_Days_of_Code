0496ff48bcbe4a59de2f08a5e78b99f3deacefea
FFudo <philipphofbauer@aon.at>
2024-11-10 19:50:32 +0100

    Day 2 Challenge completed

---
+++ Day2_TipCalc/app.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total = float(input("What was the total bill?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
+portion = round((total * (1 + tip / 100)) / people, 2)
+print(f"Each person has to pay: {portion}")
