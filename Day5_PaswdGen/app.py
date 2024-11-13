f1099f5d26dbfb76297b3f82fd7c7e86ae94af4c
FFudo <philipphofbauer@aon.at>
2024-11-13 20:56:23 +0100

    Finished Day 5 Hard

--- Day5_PaswdGen/app.py
+++ Day5_PaswdGen/app.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,3 +9,14 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+passwd = ""
+
+for nr_letter in range(0, nr_letters):
+    passwd += random.choice(letters)
+for nr_symbol in range(0, nr_symbols):
+    passwd += random.choice(symbols)
+for nr_number in range(0, nr_numbers):
+    passwd += random.choice(numbers)
+
+shuffled_passwd = "".join(random.sample(passwd, len(passwd)))
+print(shuffled_passwd)
