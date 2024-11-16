55ad81034837d5e56de290588e14b8d082192bde
FFudo <philipphofbauer@aon.at>
2024-11-16 17:50:22 +0100

    added logo and failsafe for numbers and special chars

--- Day8_CaesarCipher/app.py
+++ Day8_CaesarCipher/app.py
@@ -2,10 +2,6 @@ from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(encode_or_decode, original_text, shift_amount):
     if encode_or_decode == "decode":
         shift_amount *= -1
@@ -13,11 +9,19 @@ def caesar(encode_or_decode, original_text, shift_amount):
     output_text = ""
 
     for letter in original_text:
-        shifted_pos = alphabet.index(letter) + shift_amount
-        shifted_pos %= len(alphabet)
-        output_text += alphabet[shifted_pos]
+        if letter in alphabet:
+            shifted_pos = alphabet.index(letter) + shift_amount
+            shifted_pos %= len(alphabet)
+            output_text += alphabet[shifted_pos]
+        else:
+            output_text += letter
 
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
