99c9a83fa5f73aa36456d7394bf8e9d1018ca835
FFudo <philipphofbauer@aon.at>
2024-11-16 17:58:19 +0100

    fixed loop

--- Day8_CaesarCipher/app.py
+++ Day8_CaesarCipher/app.py
@@ -1,34 +1,36 @@
 from art import logo
 
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(encode_or_decode, original_text, shift_amount):
-    is_looping = True
-    while is_looping:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
-        output_text = ""
+    output_text = ""
 
-        for letter in original_text:
-            if letter in alphabet:
-                shifted_pos = alphabet.index(letter) + shift_amount
-                shifted_pos %= len(alphabet)
-                output_text += alphabet[shifted_pos]
-            else:
-                output_text += letter
+    for letter in original_text:
+        if letter in alphabet:
+            shifted_pos = alphabet.index(letter) + shift_amount
+            shifted_pos %= len(alphabet)
+            output_text += alphabet[shifted_pos]
+        else:
+            output_text += letter
 
-        print(f"Here is the {encode_or_decode}d result: {output_text}")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-        go_again = input("Type 'yes' if you want to go again. Otherwise, type 'no'.").lower()
+is_looping = True
 
-        if go_again == "no":
-            is_looping = False
+while is_looping:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-print(logo)
+    caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.").lower()
 
-caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
+    if restart == "no":
+        is_looping = False
+        print("Goodbye")
