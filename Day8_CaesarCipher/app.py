ce03ad9f175ed278956e9d13f75813ae97abcfd4
FFudo <philipphofbauer@aon.at>
2024-11-16 17:19:18 +0100

    added encrypt function

---
+++ Day8_CaesarCipher/app.py
@@ -0,0 +1,20 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    encoded_text = ""
+    for letter in original_text:
+        index = alphabet.index(letter)
+        if index + shift_amount > 25:
+            new_index = index + shift_amount - 26
+            encoded_text += alphabet[new_index]
+        else:
+            encoded_text += alphabet[index + shift_amount]
+    print(encoded_text)
+    
+encrypt(text, shift)
