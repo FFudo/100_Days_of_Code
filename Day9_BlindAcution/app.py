dfa36421ede0de2b3ee42fbacc5631d2de22d064
FFudo <philipphofbauer@aon.at>
2024-11-17 17:35:07 +0100

    Finished Day 9

--- Day9_BlindAcution/app.py
+++ Day9_BlindAcution/app.py
@@ -1,7 +1,29 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
 from art import logo
 
 print(logo)
+print("Welcome to the secret auction program.")
+
+loop = True
+all_bids = {}
+
+while loop:
+    name = input("What is your name?:")
+    bid = int(input("What is your bid?: $"))
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+
+    all_bids[name] = bid
+
+    if other_bidders == "no":
+        loop = False
+
+    print("\n" * 20)
+
+winner_bid = 0
+winner_name = ""
+
+for key in all_bids:
+    if all_bids[key] > winner_bid:
+        winner_bid = all_bids[key]
+        winner_name = key
+
+print(f"The winner is {winner_name} with a bid of ${winner_bid}.")
