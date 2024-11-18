b55260b5bb4392a2ecabaa96785ef280277165e7
FFudo <philipphofbauer@aon.at>
2024-11-18 19:12:32 +0100

    added main logic

--- Day10_Calc/app.py
+++ Day10_Calc/app.py
@@ -1,4 +1,4 @@
-
+from art import logo
 
 def add(n1, n2):
     return n1 + n2
@@ -22,5 +22,16 @@ calculations = {"+": add,
                 "/": divide,
                 }
 
+print(logo)
 
+number1 = float(input("Whats the first number?: "))
 
+for key in calculations:
+    print(key)
+
+operator = input("Pick an operation: ")
+number2 = float(input("Whats the next number? "))
+
+result = calculations[operator](number1, number2)
+
+print(f"{number1} {operator} {number2} = {result}")
