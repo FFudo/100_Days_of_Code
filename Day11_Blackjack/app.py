89097d12bebcaecd1ba67010a1c43725f1213890
FFudo <philipphofbauer@aon.at>
2024-11-19 22:32:08 +0100

    Day 11 worked on main game loop

--- Day11_Blackjack/app.py
+++ Day11_Blackjack/app.py
@@ -1,35 +1,60 @@
 from art import logo
 import random
-
-print(logo)
-
+# TODO implement winning logic
+# TODO impplement over 21 logic
+# TODO implement ace 1 or 11 logic
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_hand = []
-comp_hand = []
-loop = True
-
 
 def blackjack():
+    loop = True
     while loop:
-        reset_hands()
+        print(logo)
+        player_hand = []
+        comp_hand = []
         deal_cards(player_hand, 2)
         deal_cards(comp_hand, 2)
 
-        print(f"Your cards: {player_hand}, current score: {sum(player_hand)}")
-        print(f"Computers first card: {comp_hand[0]}")
+        print_status(player_hand, comp_hand)
 
         repeat = input("Type 'y' to get another card, type 'n' to pass:").lower()
 
+        while repeat == "y":
+            deal_cards(player_hand, 1)
 
-def reset_hands():
-    player_hand = []
-    comp_hand = []
+            if sum(player_hand) > 21:
+                break
+
+            print_status(player_hand, comp_hand)
+            repeat = input("   Type 'y' to get another card, type 'n' to pass:").lower()
+        
+        while sum(comp_hand) < 17:
+            deal_cards(comp_hand, 1)
+
+        display_winner(player_hand, comp_hand)
+
+        again = input("Do you want to play again? Type 'y' or 'no'.").lower()
+
+        if again == "n":
+            loop = False
 
 
 def deal_cards(hand: list, card_number: int):
     for n in range(card_number):
         hand.append(random.choice(cards))
 
+
+def print_status(p_hand, c_hand):
+    print(f"    Your cards: {p_hand}, current score: {sum(p_hand)}")
+    print(f"    Computers first card: {c_hand[0]}")
+
+
+def display_winner(p_hand, c_hand):
+    c_score = sum(c_hand)
+    p_score = sum(p_hand)
+    print(f"    Your cards: {p_hand}, final score: {p_score}")
+    print(f"    Computer cards: {c_hand}, final score: {c_score}")
+    if p_score > c_score:
+        print(  "You win! Good Job!")
 
 if __name__ == "__main__":
     blackjack()
