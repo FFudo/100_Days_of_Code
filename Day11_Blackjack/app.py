1c66b9bd08fdf13878a6d8bdc414c786added2e5
FFudo <philipphofbauer@aon.at>
2024-11-19 19:25:45 +0100

    Day 11 refactored main logic

--- Day11_Blackjack/app.py
+++ Day11_Blackjack/app.py
@@ -6,15 +6,30 @@ print(logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_hand = []
 comp_hand = []
-
-for n in range(2):
-    player_hand.append(random.choice(cards))
-    comp_hand.append(random.choice(cards))
+loop = True
 
 
-print(f"Your cards: {player_hand}, current score: {sum(player_hand)}")
-print(f"Computers first card: {comp_hand[0]}")
+def blackjack():
+    while loop:
+        reset_hands()
+        deal_cards(player_hand, 2)
+        deal_cards(comp_hand, 2)
 
-repeat = input("Type 'y' to get another card, type 'n' to pass:").lower()
+        print(f"Your cards: {player_hand}, current score: {sum(player_hand)}")
+        print(f"Computers first card: {comp_hand[0]}")
+
+        repeat = input("Type 'y' to get another card, type 'n' to pass:").lower()
 
 
+def reset_hands():
+    player_hand = []
+    comp_hand = []
+
+
+def deal_cards(hand: list, card_number: int):
+    for n in range(card_number):
+        hand.append(random.choice(cards))
+
+
+if __name__ == "__main__":
+    blackjack()
