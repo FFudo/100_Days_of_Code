fbb6a087a91f2ad8d78b98ba057b41e03dfb9929
FFudo <philipphofbauer@aon.at>
2024-11-25 16:04:23 +0100

    added still has question method

--- Day17_QuizGame/quiz_brain.py
+++ Day17_QuizGame/quiz_brain.py
@@ -5,6 +5,10 @@ class QuizBrain:
         self.question_list = question_list
 
     
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    
     def next_question(self):
         choice = input(f"Q. {self.question_number + 1}: {self.question_list[self.question_number].text} (True/False): ").lower()
         self.question_number += 1
