9626f0f6b7a5fbf5238806708595c9dac699f0cf
FFudo <philipphofbauer@aon.at>
2024-11-25 15:53:04 +0100

    added quizbrain class

---
+++ Day17_QuizGame/quiz_brain.py
@@ -0,0 +1,9 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    
+    def next_question(self):
+        choice = input(f"Q. {self.question_number}: {self.question_list[self.question_number].text} (True/False): ").lower()
