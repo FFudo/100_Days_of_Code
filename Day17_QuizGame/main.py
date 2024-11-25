08e049b698b1cf8d3692d0b9198ccdc67d17def7
FFudo <philipphofbauer@aon.at>
2024-11-25 16:18:29 +0100

    finalized

--- Day17_QuizGame/main.py
+++ Day17_QuizGame/main.py
@@ -11,3 +11,5 @@ for dict in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print(f"You've completed the quiz.\nYour final score was: {quiz.score}/{quiz.question_number}")
--- Day17_QuizGame/quiz_brain.py
+++ Day17_QuizGame/quiz_brain.py
@@ -3,12 +3,24 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
 
     
+    def check_anwser(self, answer, correct_answer):
+        if answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("You got it wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your score is {self.score}/{self.question_number + 1}\n")
+
+
     def still_has_question(self):
         return self.question_number < len(self.question_list)
 
     
     def next_question(self):
-        choice = input(f"Q. {self.question_number + 1}: {self.question_list[self.question_number].text} (True/False): ").lower()
+        user_answer = input(f"Q. {self.question_number + 1}: {self.question_list[self.question_number].text} (True/False): ")
+        self.check_anwser(user_answer, self.question_list[self.question_number].answer)
         self.question_number += 1
