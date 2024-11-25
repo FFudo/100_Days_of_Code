063ca37dada4b6c35732860b5a7c9b2bb37ca874
FFudo <philipphofbauer@aon.at>
2024-11-25 16:04:40 +0100

    added while loop

--- Day17_QuizGame/main.py
+++ Day17_QuizGame/main.py
@@ -1,8 +1,13 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 
 for dict in question_data:
     question_bank.append(Question(dict["text"], dict["answer"]))
 
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
