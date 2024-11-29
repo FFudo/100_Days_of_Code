d1a6baa6f007d751111b0828a8f9d823517717ae
FFudo <philipphofbauer@aon.at>
2024-11-29 14:07:53 +0100

    Added Collison with tail and extending snake

--- Day20and21_Snake/main.py
+++ Day20and21_Snake/main.py
@@ -30,9 +30,21 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.update_score()
-    
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
+        snake.extend()
 
-    
+    if (
+        snake.head.xcor() > 290
+        or snake.head.xcor() < -290
+        or snake.head.ycor() > 290
+        or snake.head.ycor() < -290
+    ):
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
 screen.exitonclick()
--- Day20and21_Snake/snake.py
+++ Day20and21_Snake/snake.py
@@ -1,26 +1,32 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 SPEED = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
     def __init__(self):
         self.segments = []
-        self.segment_gap = 0
-        self.creat_snake()
+        self.create_snake()
         self.head = self.segments[0]
 
-    def creat_snake(self):
-        for _ in range(3):
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.teleport(self.segment_gap, 0)
-            self.segment_gap -= 20
-            self.segments.append(new_segment)
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -37,11 +43,11 @@ class Snake:
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
-    
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
-    
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
