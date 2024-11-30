b26f13d083d1913d5561a7daa981ca00a51e3d43
FFudo <philipphofbauer@aon.at>
2024-11-30 14:50:50 +0100

    day 22 setting up screen

---
+++ Day22_Pong/main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+game_is_on = True
+
+while game_is_on:
+    pass
+
+screen.exitonclick()
