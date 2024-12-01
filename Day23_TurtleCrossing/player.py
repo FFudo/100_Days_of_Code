b0062dd4d2387e4d84b58d5c1c3d5faf247c9579
FFudo <philipphofbauer@aon.at>
2024-12-01 15:31:48 +0100

    added player movemnet and reset

--- Day23_TurtleCrossing/player.py
+++ Day23_TurtleCrossing/player.py
@@ -9,12 +9,17 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.goto(STARTING_POSITION)
         self.shape("turtle")
         self.setheading(90)
-        self.finish_line = FINISH_LINE_Y
-
+        self.reset_pos()
 
     def move_foward(self):
         self.forward(MOVE_DISTANCE)
 
+    def is_at_finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+
+    def reset_pos(self):
+        self.goto(STARTING_POSITION)
