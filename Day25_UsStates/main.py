b3ea9004433ac7b5716639e158633c97acb05ae4
FFudo <philipphofbauer@aon.at>
2024-12-03 22:23:36 +0100

    write right answer on screen

--- Day25_UsStates/main.py
+++ Day25_UsStates/main.py
@@ -14,6 +14,10 @@ correct_states_list = []
 df = pd.read_csv("./Day25_UsStates/50_states.csv")
 states_list = df["state"].to_list()
 
+tim = turtle.Turtle()
+tim.penup()
+tim.hideturtle()
+
 
 while correct_states_total < 50:
     answer_state = screen.textinput(
@@ -23,5 +27,11 @@ while correct_states_total < 50:
     if answer_state in states_list:
         correct_states_total += 1
         correct_states_list.append(answer_state)
+        correct_row = df[df["state"] == answer_state]
+        print(correct_row)
+        cords = (correct_row.x.values[0], correct_row.y.values[0])
+        tim.goto(cords)
+        tim.write(arg=answer_state, align="center", font=("Courier", 12, "normal"))
+
 
 
