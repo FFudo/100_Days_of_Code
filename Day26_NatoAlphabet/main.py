626df908b070990a465c39b219ad9341afe21c58
FFudo <philipphofbauer@aon.at>
2024-12-04 17:51:24 +0100

    Reduced code

--- Day26_NatoAlphabet/main.py
+++ Day26_NatoAlphabet/main.py
@@ -4,8 +4,6 @@ df = pd.read_csv("./Day26_NatoAlphabet/nato_phonetic_alphabet.csv")
 
 alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-word = input("Enter a word: ")
+word = input("Enter a word: ").upper()
 
-letters = [letter.upper() for letter in word]
-
-print([alphabet_dict[letter] for letter in letters])
+print([alphabet_dict[letter] for letter in word])
