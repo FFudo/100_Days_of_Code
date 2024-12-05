c8b4570c10d9cfb09533502dc1e94eed53adec03
FFudo <philipphofbauer@aon.at>
2024-12-05 20:16:11 +0100

    day 27 finished

--- Day27_Tkinter/main.py
+++ Day27_Tkinter/main.py
@@ -1,8 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My first gui program")
-window.minsize(width=500, height=300)
 
+def button_callback():
+    new_output = round(int(input.get()) * 1.609, 2)
+    output_label.config(text=new_output)
+
+
+window = Tk()
+window.title("Miles to Km Converter")
+
+
+input = Entry()
+input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+output_label = Label()
+output_label.grid(column=1, row=1)
+
+equal_label = Label(text="is euqal to")
+equal_label.grid(column=0, row=1)
+
+button = Button(text="Calculate", command=button_callback)
+button.grid(column=1, row=2)
 
 window.mainloop()
