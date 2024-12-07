61a264950d4eedc09eac5e1692db3166134f96de
FFudo <philipphofbauer@aon.at>
2024-12-07 17:38:19 +0100

    Added Ui

--- Day29_PasswordManager/main.py
+++ Day29_PasswordManager/main.py
@@ -8,9 +8,9 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 password_img = PhotoImage(file="./Day29_PasswordManager/logo.png")
 canvas.create_image(100, 100, image=password_img)
 canvas.grid(column=1, row=0)
@@ -23,5 +23,21 @@ username_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
+
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+
+username_entry = Entry(width=40)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry(width=22)
+password_entry.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
