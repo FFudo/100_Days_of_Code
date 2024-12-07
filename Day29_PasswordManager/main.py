74addd8fa011e66d45bf9db84001a21078fa6613
FFudo <philipphofbauer@aon.at>
2024-12-07 17:07:21 +0100

    Added labels

--- Day29_PasswordManager/main.py
+++ Day29_PasswordManager/main.py
@@ -13,7 +13,15 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 password_img = PhotoImage(file="./Day29_PasswordManager/logo.png")
 canvas.create_image(100, 100, image=password_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+username_label = Label(text=r"Email/Username:")
+username_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 
 window.mainloop()
