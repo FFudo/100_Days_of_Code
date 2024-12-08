678f58ea46a85c8acaf4b031df023bdc34bdc13c
FFudo <philipphofbauer@aon.at>
2024-12-08 18:05:40 +0100

    added exception loop to input

--- Day26_NatoAlphabet/main.py
+++ Day26_NatoAlphabet/main.py
@@ -4,6 +4,12 @@ df = pd.read_csv("./Day26_NatoAlphabet/nato_phonetic_alphabet.csv")
 
 alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-word = input("Enter a word: ").upper()
+while True:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [alphabet_dict[letter] for letter in word]
+        break
+    except:
+        print("Sorry, only letters in the alphabet please.")
 
-print([alphabet_dict[letter] for letter in word])
+print(output_list)
