5b9680d2d3e26b003954145025998ee93008b3e1
FFudo <philipphofbauer@aon.at>
2024-12-08 18:09:24 +0100

    formating improvement

--- Day26_NatoAlphabet/main.py
+++ Day26_NatoAlphabet/main.py
@@ -5,8 +5,8 @@ df = pd.read_csv("./Day26_NatoAlphabet/nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 while True:
+    word = input("Enter a word: ").upper()
     try:
-        word = input("Enter a word: ").upper()
         output_list = [alphabet_dict[letter] for letter in word]
         break
     except:
