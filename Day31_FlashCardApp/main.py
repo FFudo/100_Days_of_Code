b02ab8b7c61d6ff7eb922eb27736c24486319ff4
FFudo <philipphofbauer@aon.at>
2024-12-09 14:44:53 +0100

    added flip logic

--- Day31_FlashCardApp/main.py
+++ Day31_FlashCardApp/main.py
@@ -12,10 +12,22 @@ to_learn = df.to_dict(orient="records")
 
 def next_card():
     current_card = random.choice(to_learn)
-    card_canvas.itemconfig(lang_label, text="French")
+
     french_word = current_card["French"]
-    new_english = current_card["English"]
-    card_canvas.itemconfig(word_label, text=french_word)
+    english_word = current_card["English"]
+
+    card_canvas.itemconfig(lang_label, fill="black", text="French")
+    card_canvas.itemconfig(word_label, fill="black", text=french_word)
+    card_canvas.itemconfig(card_img, image=cardfront_img)
+
+    timer = window.after(3000, display_backside, english_word)
+
+
+def display_backside(word):
+    card_canvas.itemconfig(lang_label, fill="white", text="English")
+    card_canvas.itemconfig(word_label, fill="white", text=word)
+    card_canvas.itemconfig(card_img, image=cardback_img)
+    timer2 = window.after(3000, next_card)
 
 
 #### UI ####
@@ -27,12 +39,14 @@ card_canvas = Canvas(
     width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0
 )
 cardfront_img = PhotoImage(file="./Day31_FlashCardApp/images/card_front.png")
-card_canvas.create_image(400, 263, image=cardfront_img)
+cardback_img = PhotoImage(file="./Day31_FlashCardApp/images/card_back.png")
+card_img = card_canvas.create_image(400, 263, image=cardfront_img)
+
 lang_label = card_canvas.create_text(
-    400, 150, text="Title", fill="black", font=("Ariel", 40, "italic")
+    400, 150, text="", fill="black", font=("Ariel", 40, "italic")
 )
 word_label = card_canvas.create_text(
-    400, 263, text="Word", fill="black", font=("Ariel", 60, "bold")
+    400, 263, text="", fill="black", font=("Ariel", 60, "bold")
 )
 card_canvas.grid(column=0, row=0, columnspan=2)
 
@@ -48,5 +62,7 @@ right_button = Button(
 )
 right_button.grid(column=1, row=1)
 
+
+next_card()
 
 window.mainloop()
