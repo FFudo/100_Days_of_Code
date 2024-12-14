956488d6fb4eff25edcafa8844e0e653b6fec34a
FFudo <philipphofbauer@aon.at>
2024-12-14 15:12:07 +0100

    finalised ui

--- Day34_Quizzler/ui.py
+++ Day34_Quizzler/ui.py
@@ -7,16 +7,21 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(background=THEME_COLOR, padx=20, pady=20)
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        self.score_label = Label(text="Score: 0", font=("Arial", 10, "normal"), background=THEME_COLOR, fg="white")
+        self.score_label = Label(
+            text="Score: 0",
+            font=("Arial", 10, "normal"),
+            background=THEME_COLOR,
+            fg="white",
+        )
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, background="white")
-        self.canvas.create_text(
-            150, 125, text="Some Question", font=("Arial", 20, "italic")
+        self.question_text = self.canvas.create_text(
+            150, 125, text="Some Question", font=("Arial", 20, "italic"), fill=THEME_COLOR
         )
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="Day34_Quizzler/images/true.png")
         self.true_button = Button(image=true_img, borderwidth=0, highlightthickness=0)
