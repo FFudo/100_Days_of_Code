97250e44c9fafd6263bf94fa45ee74b3a8db1548
FFudo <philipphofbauer@aon.at>
2024-12-14 14:55:29 +0100

    added Ui Class

--- Day34_Quizzler/ui.py
+++ Day34_Quizzler/ui.py
@@ -1,2 +1,10 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(background=THEME_COLOR)
+
+        self.window.mainloop()
