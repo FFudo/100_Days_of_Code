34be900edd306f84456c89c5543f9aaedaf539fc
FFudo <philipphofbauer@aon.at>
2024-12-15 15:47:27 +0100

    Started Day 35

---
+++ Day35_RainAlert/main.py
@@ -0,0 +1,27 @@
+from config import api_key
+
+import requests
+
+parameters = {
+    "lat": 48.208176,
+    "lon": 16.373819,
+    "appid": api_key,
+    "cnt" : 4,
+}
+
+respone = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+respone.raise_for_status()
+data = respone.json()["list"]
+
+will_rain = False
+
+for hour in data:
+    if int(hour["weather"][0]["id"]) < 700:
+        will_rain = True
+
+if will_rain:
+    print("It will rain")
+
+
+
+
