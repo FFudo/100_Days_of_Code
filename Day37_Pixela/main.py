788700de8a1e4b4cb89892cf9b4d6be1014e127f
FFudo <philipphofbauer@aon.at>
2024-12-17 22:57:37 +0100

    finished day 37

--- Day37_Pixela/main.py
+++ Day37_Pixela/main.py
@@ -1,5 +1,7 @@
 import requests
 from config import TOKEN, USERNAME
+import datetime as dt
+
 
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
@@ -24,5 +26,35 @@ graph_params = {
 
 header = {"X-USER-TOKEN": TOKEN}
 
-response = requests.post(url=graph_endpoint, json=graph_params, headers=header)
-print(response.text)
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=header)
+# print(response.text)
+
+def post_pixel(n):
+    pixel_endpoint = f"{graph_endpoint}/{graph_params["id"]}"
+
+    today = str(dt.datetime.now().date())
+    today = today.replace("-", "")
+
+    pixel_params = {
+        "date": today,
+        "quantity": str(n),
+    }
+    response = requests.post(url=pixel_endpoint, json=pixel_params, headers=header)
+    print(response.text)
+
+def update_pixel(n, day: str):
+    pixel_endpoint = f"{graph_endpoint}/{graph_params["id"]}/{day}"
+
+    pixel_params = {
+        "quantity": str(n),
+    }
+    response = requests.put(url=pixel_endpoint, json=pixel_params, headers=header)
+    print(response.text)
+
+def delete_pixel(day: str):
+    pixel_endpoint = f"{graph_endpoint}/{graph_params["id"]}/{day}"
+    
+    response = requests.delete(url=pixel_endpoint, headers=header)
+    print(response.text)
+
+delete_pixel("20241217")
